2089823225368adc23b74cc8b080240449deb9bb
Chris Loveless <chriskloveless@gmail.com>
2023-01-16 11:57:37 -0800

    add create functionality to animals class

--- CretaceousApi/Program.cs
+++ CretaceousApi/Program.cs
@@ -1,9 +1,19 @@
+using CretaceousApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<CretaceousApiContext>(
+                  dbContextOptions => dbContextOptions
+                    .UseMySql(
+                      builder.Configuration["ConnectionStrings:DefaultConnection"],
+                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                  )
+                );
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -12,11 +22,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+else
+{
+  app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
